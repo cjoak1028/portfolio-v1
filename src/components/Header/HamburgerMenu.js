b87e277a53caaee0e1b61375f861a238f7a9c8af
David Kim <cjoak1028@gmail.com>
2023-01-30 21:32:50 -0500

    Added hamburger menu layout

---
+++ src/components/Header/HamburgerMenu.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./HamburgerMenu.module.css";
+
+const HamburgerMenu = () => {
+  return (
+    <div className={styles["hamburger-menu"]}>
+      <ul>
+        <li>01/ ABOUT</li>
+        <li>02/ SKILLS</li>
+        <li>03/ PROJECTS</li>
+        <li>04/ CONTACT</li>
+      </ul>
+      {/* <p className={styles.email}>changju.kim10@gmail.com</p> */}
+    </div>
+  );
+};
+
+export default HamburgerMenu;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.css";
 import Navigation from "./Navigation";
+import HamburgerMenu from "./HamburgerMenu";
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
     <div className={styles.header}>
-      <p className={styles.logo}>CJ PORTFOLIO</p>
+      <p className={`${styles.logo} ${openMenu ? "white" : ""}`}>
+        CJ PORTFOLIO
+      </p>
       <Navigation />
-      <button className={styles["hamburger-button"]}>Menu</button>
+      <button
+        className={`${styles["hamburger-button"]} ${openMenu ? "white" : ""}`}
+        onClick={toggleMenu}
+      >
+        {openMenu ? "close" : "menu"}
+      </button>
+      {openMenu ? <HamburgerMenu /> : ""}
     </div>
   );
 };
