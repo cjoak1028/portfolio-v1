8a6340569c50407b670ec9c968367f7489e1a795
David Kim <cjoak1028@gmail.com>
2023-01-31 19:25:25 -0500

    Added time to hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,22 @@
-import React from "react";
+import { toBePartiallyChecked } from "@testing-library/jest-dom/dist/matchers";
+import React, { useState, useEffect } from "react";
 import styles from "./Hero.module.css";
 
 const Hero = () => {
+  const [date, setDate] = useState(new Date());
+
+  const tick = () => {
+    setDate(new Date());
+  };
+
+  useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
+
+    return () => {
+      clearInterval(timerID);
+    };
+  }, []);
+
   return (
     <div
       className={`${styles.hero} padding-x flex flex-d-c flex-ai-c flex-jc-c`}
@@ -9,7 +24,13 @@ const Hero = () => {
       <div>
         <p className={styles.role}>FRONT-END DEVELOPER</p>
         <h1>CHANG-JU</h1>
-        <p className={styles.location}>TORONTO -- 01:23:34 PM</p>
+        <p className={styles.location}>
+          TORONTO --{" "}
+          <span className={styles.time}>
+            {/* <span>01</span>:<span>23</span>:<span>34</span> <span>PM</span> */}
+            {date.toLocaleTimeString()}
+          </span>
+        </p>
       </div>
     </div>
   );
