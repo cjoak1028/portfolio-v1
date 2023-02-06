06e5fc3c55c80b82525d1f5c0845403bc07c2042
David Kim <cjoak1028@gmail.com>
2023-02-05 21:11:06 -0500

    Added markup for skills component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Section from "./components/Layout/Section";
 import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         <Hero />
         <Section title={"01/ about"} rotate={true}>
           <About />
+        </Section>
+        <Section title={"02/ skills"} rotate={false}>
+          <Skills />
         </Section>
       </div>
     </div>
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./Skills.module.css";
+
+const Skills = () => {
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>JavaScript (ES6)</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>SCSS/SASS</li>
+          <li>Python</li>
+          <li>Java</li>
+          <li>C</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>React</li>
+          <li>Node.JS</li>
+          <li>Express</li>
+          <li>Mongoose</li>
+          <li>React Native</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>Git</li>
+          <li>Webpack</li>
+          <li>MongoDB</li>
+          <li>Figma</li>
+          <li>Adobe Photoshop</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
