bcb8c29e7613217d9c02766300efdb230d75d3ba
David Kim <cjoak1028@gmail.com>
2023-02-06 23:34:27 -0500

    Finished skills section for desktop

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,37 +1,42 @@
 import React from "react";
 import styles from "./Skills.module.css";
+import { AiOutlineSmile } from "react-icons/ai";
 
 const Skills = () => {
   return (
-    <div>
-      <div>
-        <ul>
-          <li>JavaScript (ES6)</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>SCSS/SASS</li>
-          <li>Python</li>
-          <li>Java</li>
-          <li>C</li>
-        </ul>
+    <div className={styles.skills}>
+      <div className={styles["inf-scroll"]}>
+        <span>JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -</span>
+        <span>JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -</span>
+        <span>JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -</span>
+        <span>JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -</span>
+        <span>JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -</span>
+        <span>JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -</span>
       </div>
-      <div>
-        <ul>
-          <li>React</li>
-          <li>Node.JS</li>
-          <li>Express</li>
-          <li>Mongoose</li>
-          <li>React Native</li>
-        </ul>
+      <div className={styles["heading-container"]}>
+        <h3>Languages</h3>
       </div>
-      <div>
-        <ul>
-          <li>Git</li>
-          <li>Webpack</li>
-          <li>MongoDB</li>
-          <li>Figma</li>
-          <li>Adobe Photoshop</li>
-        </ul>
+      <div className={styles["inf-scroll-rev"]}>
+        <span>- React - Node.JS - Express - Mongoose - React Native</span>
+        <span>- React - Node.JS - Express - Mongoose - React Native</span>
+        <span>- React - Node.JS - Express - Mongoose - React Native</span>
+        <span>- React - Node.JS - Express - Mongoose - React Native</span>
+        <span>- React - Node.JS - Express - Mongoose - React Native</span>
+        <span>- React - Node.JS - Express - Mongoose - React Native</span>
+      </div>
+      <div className={styles["heading-container"]}>
+        <h3>LIBRARIES/FRAMEWORKS</h3>
+      </div>
+      <div className={styles["inf-scroll"]}>
+        <span>Git - Webpack - MongoDB - Figma - Photoshop -</span>
+        <span>Git - Webpack - MongoDB - Figma - Photoshop -</span>
+        <span>Git - Webpack - MongoDB - Figma - Photoshop -</span>
+        <span>Git - Webpack - MongoDB - Figma - Photoshop -</span>
+        <span>Git - Webpack - MongoDB - Figma - Photoshop -</span>
+        <span>Git - Webpack - MongoDB - Figma - Photoshop -</span>
+      </div>
+      <div className={styles["heading-container"]}>
+        <h3>Other Tools</h3>
       </div>
     </div>
   );
