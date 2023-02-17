98d306c2e4c99c31e3cb4665389c759dc57eddf8
David Kim <cjoak1028@gmail.com>
2023-02-17 14:42:43 -0500

    Made hero responsive

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -20,21 +20,55 @@ const Hero = () => {
 
   return (
     <div className={styles.hero}>
-      <div className={styles["hero-text"]}>
-        <div className={styles.about}>
-          <p className={styles.name}>Chang-Ju Kim</p>
-          <p>Front-End Developer</p>
-          <p className={styles.location}>
-            TORONTO &#8212;{" "}
-            <span className={styles.time}>{date.toLocaleTimeString()}</span>
-          </p>
+      <div className={styles["hero-container"]}>
+        <div className={styles["inf-scroll"]}>
+          <span className={styles.name}>CHANG-JU KIM</span>
+          <span className={styles.divider}>X</span>
+          <span className={styles.name}>CHANG-JU KIM</span>
+          <span className={styles.divider}>X</span>
+          <span className={styles.name}>CHANG-JU KIM </span>
+          <span className={styles.divider}>X</span>
+          <span className={styles.name}>CHANG-JU KIM</span>
+          <span className={styles.divider}>X</span>
+          <span className={styles.name}>CHANG-JU KIM </span>
+          <span className={styles.divider}>X</span>
+          <span className={styles.name}>CHANG-JU KIM</span>
+          <span className={styles.divider}>X</span>
+          <span className={styles.name}>CHANG-JU KIM </span>
+          <span className={styles.divider}>X</span>
+          <span className={styles.name}>CHANG-JU KIM</span>
+          <span className={styles.divider}>X</span>
+          <span className={styles.name}>CHANG-JU KIM</span>
+          <span className={styles.divider}>X</span>
+          <span className={styles.name}>CHANG-JU KIM </span>
+          <span className={styles.divider}>X</span>
+          <span className={styles.name}>CHANG-JU KIM</span>
+          <span className={styles.divider}>X</span>
+          <span className={styles.name}>CHANG-JU KIM </span>
+          <span className={styles.divider}>X</span>
+          <span className={styles.name}>CHANG-JU KIM</span>
+          <span className={styles.divider}>X</span>
+          <span className={styles.name}>CHANG-JU KIM </span>
+          <span className={styles.divider}>X</span>
         </div>
-        <div className={styles.contact}>
-          <p className={styles.email}>changju.kim10@gmail.com</p>
-          <p>905 580 9682</p>
+        <div className={styles["hero-image"]}></div>
+        <div className={styles["hero-text"]}>
+          <div className={styles.about}>
+            {/* <p className={styles.name}>Chang-Ju Kim</p> */}
+            <div className={styles["flex-container"]}>
+              <p className={styles.position}>Front-End Developer</p>
+              <p className={styles.location}>
+                TORONTO &#8212;{" "}
+                <span className={styles.time}>{date.toLocaleTimeString()}</span>
+              </p>
+            </div>
+          </div>
+          <div className={styles.contact}>
+            <p className={styles.email}>changju.kim10@gmail.com</p>
+            <p>905 580 9682</p>
+          </div>
         </div>
       </div>
-      <div className={styles["hero-image"]}></div>
     </div>
   );
 };
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -8,12 +8,24 @@ const Skills = () => {
         <div
           className={`${styles["inf-scroll"]} ${styles["inf-scroll--left"]}`}
         >
-          <span>Javascript - HTML - CSS - SCSS/SASS - Python - Java - C </span>
-          <span>JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -</span>
-          <span>JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -</span>
-          <span>JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -</span>
-          <span>JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -</span>
-          <span>JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -</span>
+          <span className={styles.group}>
+            Javascript - HTML - CSS - SCSS/SASS - Python - Java - C -
+          </span>
+          <span className={styles.group}>
+            JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -
+          </span>
+          <span className={styles.group}>
+            JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -
+          </span>
+          <span className={styles.group}>
+            JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -
+          </span>
+          <span className={styles.group}>
+            JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -
+          </span>
+          <span className={styles.group}>
+            JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -
+          </span>
         </div>
         <h3>Languages</h3>
       </div>
@@ -21,12 +33,24 @@ const Skills = () => {
         <div
           className={`${styles["inf-scroll"]} ${styles["inf-scroll--right"]}`}
         >
-          <span>- React - Node.JS - Express - Mongoose - React Native</span>
-          <span>- React - Node.JS - Express - Mongoose - React Native</span>
-          <span>- React - Node.JS - Express - Mongoose - React Native</span>
-          <span>- React - Node.JS - Express - Mongoose - React Native</span>
-          <span>- React - Node.JS - Express - Mongoose - React Native</span>
-          <span>- React - Node.JS - Express - Mongoose - React Native</span>
+          <span className={styles.group}>
+            - React - Node.JS - Express - Mongoose - React Native
+          </span>
+          <span className={styles.group}>
+            - React - Node.JS - Express - Mongoose - React Native
+          </span>
+          <span className={styles.group}>
+            - React - Node.JS - Express - Mongoose - React Native
+          </span>
+          <span className={styles.group}>
+            - React - Node.JS - Express - Mongoose - React Native
+          </span>
+          <span className={styles.group}>
+            - React - Node.JS - Express - Mongoose - React Native
+          </span>
+          <span className={styles.group}>
+            - React - Node.JS - Express - Mongoose - React Native
+          </span>
         </div>
         <h3>Libraries/Frameworks</h3>
       </div>
@@ -34,12 +58,24 @@ const Skills = () => {
         <div
           className={`${styles["inf-scroll"]} ${styles["inf-scroll--left"]}`}
         >
-          <span>Git - Webpack - MongoDB - Figma - Photoshop -</span>
-          <span>Git - Webpack - MongoDB - Figma - Photoshop -</span>
-          <span>Git - Webpack - MongoDB - Figma - Photoshop -</span>
-          <span>Git - Webpack - MongoDB - Figma - Photoshop -</span>
-          <span>Git - Webpack - MongoDB - Figma - Photoshop -</span>
-          <span>Git - Webpack - MongoDB - Figma - Photoshop -</span>
+          <span className={styles.group}>
+            Git - Webpack - MongoDB - Figma - Photoshop -
+          </span>
+          <span className={styles.group}>
+            Git - Webpack - MongoDB - Figma - Photoshop -
+          </span>
+          <span className={styles.group}>
+            Git - Webpack - MongoDB - Figma - Photoshop -
+          </span>
+          <span className={styles.group}>
+            Git - Webpack - MongoDB - Figma - Photoshop -
+          </span>
+          <span className={styles.group}>
+            Git - Webpack - MongoDB - Figma - Photoshop -
+          </span>
+          <span className={styles.group}>
+            Git - Webpack - MongoDB - Figma - Photoshop -
+          </span>
         </div>
         <h3>Other Tools</h3>
       </div>
