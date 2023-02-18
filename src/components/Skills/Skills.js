2c563c0669c5861875644c33e8283210365e86a4
David Kim <cjoak1028@gmail.com>
2023-02-18 16:13:30 -0500

    Fixed skills bug

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -8,74 +8,192 @@ const Skills = () => {
         <div
           className={`${styles["inf-scroll"]} ${styles["inf-scroll--left"]}`}
         >
-          <span className={styles.group}>
-            Javascript - HTML - CSS - SCSS/SASS - Python - Java - C -
-          </span>
-          <span className={styles.group}>
-            JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -
-          </span>
-          <span className={styles.group}>
-            JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -
-          </span>
-          <span className={styles.group}>
-            JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -
-          </span>
-          <span className={styles.group}>
-            JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -
-          </span>
-          <span className={styles.group}>
-            JavaScript - HTML - CSS - SCSS/SASS - Python - Java - C -
-          </span>
+          <span className={styles.skill}>JS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>HTML</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>CSS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>SCSS/SASS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Python</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Java</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>C</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>JS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>HTML</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>CSS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>SCSS/SASS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Python</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Java</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>C</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>JS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>HTML</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>CSS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>SCSS/SASS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Python</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Java</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>C</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>JS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>HTML</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>CSS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>SCSS/SASS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Python</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Java</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>C</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>JS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>HTML</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>CSS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>SCSS/SASS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Python</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Java</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>C</span>
+          <span className={styles.divider}>x</span>
         </div>
-        <h3>Languages</h3>
+        <h3>LANGUAGES</h3>
       </div>
+
       <div className={styles["skills__category"]}>
         <div
           className={`${styles["inf-scroll"]} ${styles["inf-scroll--right"]}`}
         >
-          <span className={styles.group}>
-            - React - Node.JS - Express - Mongoose - React Native
-          </span>
-          <span className={styles.group}>
-            - React - Node.JS - Express - Mongoose - React Native
-          </span>
-          <span className={styles.group}>
-            - React - Node.JS - Express - Mongoose - React Native
-          </span>
-          <span className={styles.group}>
-            - React - Node.JS - Express - Mongoose - React Native
-          </span>
-          <span className={styles.group}>
-            - React - Node.JS - Express - Mongoose - React Native
-          </span>
-          <span className={styles.group}>
-            - React - Node.JS - Express - Mongoose - React Native
-          </span>
+          <span className={styles.skill}>React</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Node.JS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Express</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Mongoose</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>React Native</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>React</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Node.JS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Express</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Mongoose</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>React Native</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>React</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Node.JS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Express</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Mongoose</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>React Native</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>React</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Node.JS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Express</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Mongoose</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>React Native</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>React</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Node.JS</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Express</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Mongoose</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>React Native</span>
+          <span className={styles.divider}>x</span>
         </div>
         <h3>Libraries/Frameworks</h3>
       </div>
+
       <div className={styles["skills__category"]}>
         <div
           className={`${styles["inf-scroll"]} ${styles["inf-scroll--left"]}`}
         >
-          <span className={styles.group}>
-            Git - Webpack - MongoDB - Figma - Photoshop -
-          </span>
-          <span className={styles.group}>
-            Git - Webpack - MongoDB - Figma - Photoshop -
-          </span>
-          <span className={styles.group}>
-            Git - Webpack - MongoDB - Figma - Photoshop -
-          </span>
-          <span className={styles.group}>
-            Git - Webpack - MongoDB - Figma - Photoshop -
-          </span>
-          <span className={styles.group}>
-            Git - Webpack - MongoDB - Figma - Photoshop -
-          </span>
-          <span className={styles.group}>
-            Git - Webpack - MongoDB - Figma - Photoshop -
-          </span>
+          <span className={styles.skill}>Git</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Webpack</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>MongoDB</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Figma</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Photoshop</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Git</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Webpack</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>MongoDB</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Figma</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Photoshop</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Git</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Webpack</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>MongoDB</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Figma</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Photoshop</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Git</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Webpack</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>MongoDB</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Figma</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Photoshop</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Git</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Webpack</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>MongoDB</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Figma</span>
+          <span className={styles.divider}>x</span>
+          <span className={styles.skill}>Photoshop</span>
+          <span className={styles.divider}>x</span>
         </div>
         <h3>Other Tools</h3>
       </div>
