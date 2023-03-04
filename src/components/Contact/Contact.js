177955c9cb27d0f304801942df1b08c47d36ed34
David Kim <cjoak1028@gmail.com>
2023-03-04 17:26:31 -0500

    Changed background color and fixed margins for contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,28 +4,30 @@ import styles from "./Contact.module.css";
 const Contact = () => {
   return (
     <div className={`${styles.contact} padding-x`}>
-      <h3>HIRE ME!</h3>
-      <p>
-        or feel free to message me if you want to collaborate or have a little
-        chat.
-      </p>
-      <span className={styles.divider}></span>
-      <ul className={styles["link-list"]}>
-        <li>
-          <a>Email</a>
-        </li>
-        <li>
-          <a href={"https://linkedin.com/in/chang-ju-kim-966351255"}>
-            LinkedIn
-          </a>
-        </li>
-        <li>
-          <a href={"https://github.com/cjoak1028"}>Github</a>
-        </li>
-        <li>
-          <a href={""}>Resume</a>
-        </li>
-      </ul>
+      <div className={styles["contact__content"]}>
+        <h3>HIRE ME!</h3>
+        <p>
+          or feel free to message me if you want to collaborate or have a little
+          chat.
+        </p>
+        <span className={styles.divider}></span>
+        <ul className={styles["link-list"]}>
+          <li>
+            <a>Email</a>
+          </li>
+          <li>
+            <a href={"https://linkedin.com/in/chang-ju-kim-966351255"}>
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a href={"https://github.com/cjoak1028"}>Github</a>
+          </li>
+          <li>
+            <a href={""}>Resume</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
