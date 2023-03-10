92f743452652326e79a682f648bdf36ac55a836d
David Kim <cjoak1028@gmail.com>
2023-03-10 12:23:55 -0500

    Added vertical scrolling for desktop view

--- src/App.js
+++ src/App.js
@@ -6,24 +6,33 @@ import Contact from "./components/Contact/Contact";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer/Footer";
 import { BsArrowDownShort } from "react-icons/bs";
+import { Element } from "react-scroll";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="padding-x">
-        <Hero />
-        <About />
+        <Element name="hero">
+          <Hero />
+        </Element>
+        <Element name="about">
+          <About />
+        </Element>
         <div className={styles.prompt}>
           <p>Check out some of my work </p>
           <BsArrowDownShort className={styles["arrow-icon"]} />
         </div>
-        <Projects />
+        <Element name="projects">
+          <Projects />
+        </Element>
         <div className={`${styles.prompt} ${styles["prompt--rev"]}`}>
           <BsArrowDownShort className={styles["arrow-icon"]} />
           <p>Get in touch </p>
         </div>
-        <Contact />
+        <Element name="contact">
+          <Contact />
+        </Element>
       </div>
       <Footer />
       <div className={styles["side-element"]}>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-scroll";
 import styles from "./Header.module.css";
-import Navigation from "./Navigation";
 import HamburgerMenu from "./HamburgerMenu";
 
 const Header = () => {
@@ -23,8 +23,50 @@ const Header = () => {
 
   return (
     <div className={`${styles.header} padding-x flex flex-jc-sb flex-ai-c`}>
-      <p className={styles.logo}>CJ PORTFOLIO</p>
-      <Navigation />
+      <p className={styles.logo}>
+        <Link to="hero" spy={true} smooth={true} duration={500} offset={-70}>
+          CJ PORTFOLIO
+        </Link>
+      </p>
+      <nav className={styles.navigation}>
+        <ul>
+          <li>
+            <Link
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={-110}
+            >
+              About
+            </Link>
+          </li>
+          <li>-</li>
+          <li>
+            <Link
+              to="projects"
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={-110}
+            >
+              Projects
+            </Link>
+          </li>
+          <li>-</li>
+          <li>
+            <Link
+              to="contact"
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={-110}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <button
         className={`${styles["hamburger-button"]} ${
           isButtonDisabled ? styles.disabled : ""
