2699a02ed767025632834fa45ecdfb5daa88f9bd
David Kim <cjoak1028@gmail.com>
2023-03-26 14:09:30 -0400

    Added animations to contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,27 +3,55 @@ import { motion } from "framer-motion";
 import styles from "./Contact.module.css";
 import Section from "../Layout/Section";
 
+const contactEl = {
+  visible: {
+    opacity: 1,
+    transition: { duration: 0.6, delay: 0.2 },
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+
+const linksList = {
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  hidden: { opacity: 0 },
+};
+
+const linksItem = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 160,
+    },
+  },
+  hidden: { opacity: 0, y: 10 },
+};
+
 const Contact = () => {
   return (
     <Section name={"contact"} title={"contact"}>
       <div className={`${styles.contact}`}>
         <div className={styles["contact__content"]}>
           <motion.h3
-            initial={{ opacity: 0 }}
-            whileInView={{
-              opacity: 1,
-              transition: { duration: 0.6, delay: 0.2 },
-            }}
+            variants={contactEl}
+            initial="hidden"
+            whileInView="visible"
             viewport={{ once: true }}
           >
             HIRE ME
           </motion.h3>
           <motion.p
-            initial={{ opacity: 0 }}
-            whileInView={{
-              opacity: 1,
-              transition: { duration: 0.6, delay: 0.2 },
-            }}
+            variants={contactEl}
+            initial="hidden"
+            whileInView="visible"
             viewport={{ once: true }}
           >
             or feel free to message me if you want to collaborate or have a
@@ -32,29 +60,27 @@ const Contact = () => {
           <span className={styles.divider}></span>
           <motion.ul
             className={styles["link-list"]}
-            initial={{ opacity: 0 }}
-            whileInView={{
-              opacity: 1,
-              transition: { duration: 0.6, delay: 0.2 },
-            }}
+            variants={linksList}
+            initial="hidden"
+            whileInView="visible"
             viewport={{ once: true }}
           >
-            <li>
+            <motion.li variants={linksItem}>
               <a>Email</a>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li variants={linksItem}>
               <a href={"https://linkedin.com/in/chang-ju-kim-966351255"}>
                 LinkedIn
               </a>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li variants={linksItem}>
               <a href={"https://github.com/cjoak1028"}>Github</a>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li variants={linksItem}>
               <a href={"/resume.pdf"} target="_blank" rel="noopener noreferrer">
                 Resume
               </a>
-            </li>
+            </motion.li>
           </motion.ul>
         </div>
       </div>
