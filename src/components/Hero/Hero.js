6a15994615abd343087fbdad4d35f81437aa7d52
David Kim <cjoak1028@gmail.com>
2023-04-05 19:35:53 -0400

    Re-designed hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -48,7 +48,7 @@ const fromTop = {
 
 const Hero = () => {
   return (
-    <Div100vh className={styles["hero-container"]}>
+    <div className={styles["hero-container"]}>
       <motion.div
         initial="hidden"
         animate="visible"
@@ -57,39 +57,49 @@ const Hero = () => {
       >
         <div className={styles["hero__content"]}>
           <div className={styles["hero__section-1"]}>
-            <motion.p variants={fromBottom} className={styles.greeting}>
-              Hello,
-            </motion.p>
+            <div className={styles.intro}>
+              <motion.p variants={fromBottom} className={styles.greeting}>
+                Hello, my name is
+              </motion.p>
+              <motion.p variants={fromBottom} className={styles.name}>
+                CHANG-JU.
+              </motion.p>
+            </div>
             <div>
               <motion.div
                 variants={fromBottom}
                 className={styles["hero-image"]}
               ></motion.div>
+              {/* <div className={styles["hero__footer"]}>
+                <motion.div
+                  variants={fromTop}
+                  className={styles["scroll-prompt"]}
+                >
+                  <BsArrowDownShort className={styles["arrow-icon"]} />
+                  <p>Front-end Developer</p>
+                </motion.div>
+                <motion.div variants={fromTop} className={styles.time}>
+                  <Time />
+                </motion.div>
+              </div> */}
             </div>
           </div>
           <div className={styles["hero__section-2"]}>
-            <motion.p variants={fromBottom} className={styles.greeting}>
-              Hello,
-            </motion.p>
-            <div>
-              <motion.p variants={fromBottom}>My name is Chang-Ju.</motion.p>
-              <motion.p variants={fromBottom} className={styles.title}>
-                I am a front-end developer.
+            <div className={styles.intro}>
+              <motion.p variants={fromBottom} className={styles.greeting}>
+                Hello, my name is
+              </motion.p>
+              <motion.p variants={fromBottom} className={styles.name}>
+                CHANG-JU.
               </motion.p>
             </div>
+            <motion.p variants={fromBottom} className={styles.title}>
+              I am a front-end developer.
+            </motion.p>
           </div>
         </div>
-        <div className={styles["hero__footer"]}>
-          <motion.div variants={fromTop} className={styles["scroll-prompt"]}>
-            <BsArrowDownShort className={styles["arrow-icon"]} />
-            <p>Scroll Down</p>
-          </motion.div>
-          <motion.div variants={fromTop} className={styles.time}>
-            <Time />
-          </motion.div>
-        </div>
       </motion.div>
-    </Div100vh>
+    </div>
   );
 };
 
