d455f76eda887e1f0c791df10835aaddfdf93aae
David Kim <cjoak1028@gmail.com>
2023-04-06 22:01:54 -0400

    Removed smiley face

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,9 +18,12 @@ const About = () => {
           }}
           viewport={{ once: true }}
         >
-          <p className={styles.greeting}>
+          {/* <p className={styles.greeting}>
             Hello <AiOutlineSmile className={styles["smiley-icon"]} /> &#8212;
             I'm Chang-Ju (David) Kim.
+          </p> */}
+          <p className={styles.greeting}>
+            Hello &#8212; I'm Chang-Ju (David) Kim.
           </p>
           <p>
             As a front-end developer, I am driven by a desire to create digital
